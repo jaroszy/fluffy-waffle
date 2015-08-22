8c211911d9507fdfaa2737d5c386638ed1cf16af
jarek <jaroslaw.berlowski@gmail.com>
2015-08-22 19:37:24 +0200

    Gradle project, ping functionality

---
+++ src/main/java/org/jj/fluffuywaffle/Application.java
@@ -0,0 +1,35 @@
+package org.jj.fluffuywaffle;
+
+import org.jj.fluffuywaffle.jobs.CheckConnectivityJob;
+import org.quartz.*;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Application {
+
+    public static void main(String... args) {
+        try {
+            SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
+            Scheduler sched = schedFact.getScheduler();
+            sched.start();
+            // define the job and tie it to our HelloJob class
+            JobDetail job = newJob(CheckConnectivityJob.class)
+                    .withIdentity("connectivityJob", "group1")
+                    .build();
+            // Trigger the job to run now, and then every 40 seconds
+            Trigger trigger = newTrigger()
+                    .withIdentity("myTrigger", "group1")
+                    .startNow()
+                    .withSchedule(simpleSchedule()
+                            .withIntervalInSeconds(15)
+                            .repeatForever())
+                    .build();
+            // Tell quartz to schedule the job using our trigger
+            sched.scheduleJob(job, trigger);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+}
